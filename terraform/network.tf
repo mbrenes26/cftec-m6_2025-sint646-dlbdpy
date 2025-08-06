268ec0ab4c741cf9095b5b53a8edd96a2de14dc0
Mario Brenes <mbrenes26@gmail.com>
2025-08-05 21:10:56 -0600

    Completa definición de red con VNet, Subnet, Public IP y NIC

--- terraform/network.tf
+++ terraform/network.tf
@@ -5,3 +5,28 @@ resource "azurerm_public_ip" "lab_pip" {
   allocation_method   = "Static"
   sku                 = "Standard"
 }
+resource "azurerm_network_interface" "lab_nic" {
+  name                = "nic-cftec-m62025-SINT646-lab01"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.lab.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.lab_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.lab_pip.id
+  }
+}
+resource "azurerm_virtual_network" "lab_vnet" {
+  name                = "vnet-cftec-m62025-SINT646-lab01"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = azurerm_resource_group.lab.name
+}
+
+resource "azurerm_subnet" "lab_subnet" {
+  name                 = "subnet-cftec-m62025-SINT646-lab01"
+  resource_group_name  = azurerm_resource_group.lab.name
+  virtual_network_name = azurerm_virtual_network.lab_vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
