90a3dd18d985f50b0f54ae221ba508fe0170fa44
Mario Brenes <mbrenes26@gmail.com>
2025-08-05 20:51:51 -0600

    Actualiza workflow Terraform para ejecutar en carpeta terraform

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name   = "rg-cftec-sint-646-tfstate"
-    storage_account_name  = "sint646sttfstatecenfotec"
-    container_name        = "tfstate"
-    key                   = "cftec-m6_2025-sint646-dlbdpy.tfstate"
+    resource_group_name  = "rg-cftec-sint-646-tfstate"
+    storage_account_name = "sint646sttfstatecenfotec"
+    container_name       = "tfstate"
+    key                  = "cftec-m6_2025-sint646-dlbdpy.tfstate"
   }
 }
