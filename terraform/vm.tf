b39e7151965757e9f4309115676cdb9eae4c606c
Mario Brenes <mbrenes26@gmail.com>
2025-08-17 15:04:55 -0600

    feat: vm with cloud-init (docker+tmux+python+notebook6 preinstalled)

--- terraform/vm.tf
+++ terraform/vm.tf
@@ -1,12 +1,11 @@
 resource "azurerm_linux_virtual_machine" "lab_vm" {
-  name                = "vm-cftec-m62025-SINT646-lab01"
-  resource_group_name = azurerm_resource_group.lab.name
-  location            = var.location
-  size                = "Standard_A4m_v2"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.lab_nic.id
-  ]
+  name                            = "vm-cftec-m62025-SINT646-labs"
+  resource_group_name             = azurerm_resource_group.lab.name
+  location                        = var.location
+  size                            = "Standard_A4m_v2"
+  admin_username                  = "azureuser"
+  network_interface_ids           = [azurerm_network_interface.lab_nic.id]
+  disable_password_authentication = true
 
   admin_ssh_key {
     username   = "azureuser"
@@ -25,15 +24,8 @@ resource "azurerm_linux_virtual_machine" "lab_vm" {
     version   = "latest"
   }
 
-  custom_data = base64encode(<<EOF
-#!/bin/bash
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y docker.io
-systemctl enable docker
-systemctl start docker
-EOF
-  )
+  # cloud-init desde archivo externo
+  custom_data = filebase64("${path.module}/cloud-init.yml")
 
   tags = {
     project = var.project_name
