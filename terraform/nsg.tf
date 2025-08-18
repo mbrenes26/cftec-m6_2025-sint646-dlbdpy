4912639b01a1a20bf9e48795068499e2b815bbea
Mario Brenes <mbrenes26@gmail.com>
2025-08-18 01:01:39 -0600

    Add Step04 - Init Data Plane script and workflow

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -109,15 +109,15 @@ resource "azurerm_network_security_group" "lab_nsg" {
     destination_address_prefix = "*"
   }
 
-  # Kafka Broker (PLAINTEXT) - only your IP
+  # Kafka external listener (PLAINTEXT) on 29092 - only your IP
   security_rule {
-    name                       = "Allow-Kafka-MyIP"
+    name                       = "Allow-Kafka-External-MyIP"
     priority                   = 180
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "9092"
+    destination_port_range     = "29092"
     source_address_prefix      = "190.108.74.42/32"
     destination_address_prefix = "*"
   }
